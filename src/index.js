23c4c97a9e88896cb52e2c171b604c88362bb7ee
ana-alleva <aalleva@atixlabs.com>
2022-07-29 19:31:43 +0200

    Create a constructor function, initialized the state object

--- src/index.js
+++ src/index.js
@@ -1,8 +1,15 @@
 import React from "react";
 import ReactDom from "react-dom";
-import SeasonDisplay from "./SeasonDisplay";
 
 class App extends React.Component {
+  // We have to create a constructor that overrides a React component constructor. So we have to call super props because if not there is going to be an error
+  constructor(props) {
+    super(props);
+
+    //Inicializar state object
+    this.state = { lat: null };
+  }
+  // React says we have to define a Render method.
   render() {
     window.navigator.geolocation.getCurrentPosition(
       (position) => console.log(position),
